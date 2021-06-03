4ba34ae84a4d4530429a79bb9d6b9cdcf3ec4040
frrfdev <feliperenanrf23@gmail.com>
2021-06-03 18:30:32 -0300

    Primeiro Commit

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import RepositoryItem from "./RepositoryItem";
+
+import "../styles/repositories.scss";
+
+interface Repository {
+  name: string;
+  description: string;
+  url: string;
+}
+
+export default function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositorios</h1>
+
+      <ul>
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
+      </ul>
+    </section>
+  );
+}
